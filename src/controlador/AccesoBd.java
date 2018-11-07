5079bafccc96f1b311d61d792dbf0c45791da638
cyane <abelhermosoespada@gmail.com>
2018-11-07 18:02:11 +0100

    empezando v4

---
+++ src/controlador/AccesoBd.java
@@ -0,0 +1,7 @@
+package controlador;
+public class AccesoBd {
+	private String usuario = "root";
+	private String password = "";
+	private String nombreDB = "jdbc:mysql://localhost/libreria";
+	private String driver = "com.mysql.jdbc.Driver";
+}
